12d62dd28ea57e100cfc3f45324022bc91774972
Shuhei Hayakawa <shuhei.hayakawa.c8@tohoku.ac.jp>
2025-04-20 16:39:50 +0900

    Update.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,1 +1,8 @@
+(setq line-number-mode t)
+(setq column-number-mode t)
 (global-set-key "\C-h" `delete-backward-char)
+(global-set-key "\C-\\" 'other-window)
+(add-hook 'python-mode-hook
+  (lambda ()
+    (setq python-indent-offset 2)
+    (setq indent-tabs-mode nil)))
